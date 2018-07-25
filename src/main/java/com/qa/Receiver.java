84f586d57933b5966b5dd277b9eb5d53e49be91e
george <ga.timbrell@googlemail.com>
2018-07-25 09:31:50 +0100

    first commit

--- src/main/java/com/qa/Receiver.java
+++ src/main/java/com/qa/Receiver.java
@@ -1,11 +1,18 @@
 package com.qa;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
 
 @Component
 public class Receiver {
 
-    public void receiveMessage(String message) {
-        System.out.println("Received <" + message + ">");
+    public void receiveMessage(String message)
+
+    {
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject("http://localhost:9000/employeeservice/employees/{employee}", String.class, message);
+
+        System.out.println("result = <" + result + ">");
     }
 }
